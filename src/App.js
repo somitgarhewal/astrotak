ac5747ded5408281f61b445ec08496c06d0fa7d5
somitgarhewal <somitgarhewal@gmail.com>
2022-02-24 16:26:31 +0530

    add profile route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import AskQuestion from "./components/askQuestion";
+import Profile from "./components/profile";
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<AskQuestion />} />
+      <Route path='/profile' element={<Profile />} />
     </Routes>
   );
 }
